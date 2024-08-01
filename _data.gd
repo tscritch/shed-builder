ecccf4fa7b389c6f2cb2584510c9ea431c264ac0
Tad Scritchfield <tadscritch@gmail.com>
2024-07-31 23:59:05 -0500

    add board component

--- _data.gd
+++ _data.gd
@@ -1,8 +1,18 @@
 extends Node
 
+
 var ui_values = {
-	width = 1,
-	depth = 1
+	base_dim = {
+		x = 1.0, # width
+		y = 2.0, # height
+		z = 1.0, # depth
+	},
+	
+	board_dim = {
+		x = 1, # length
+		y = 0.0381,
+		z = 0.0889, 
+	}
 }
 
 # Called when the node enters the scene tree for the first time.
@@ -24,8 +34,8 @@ func _process(delta: float) -> void:
 
 func _on_width_change(new_width):
 	print("width ", new_width)
-	ui_values["width"] = new_width
+	ui_values.base_dim.x = new_width
 
 func _on_depth_change(new_depth):
 	print("depth ", new_depth)
-	ui_values["depth"] = new_depth
+	ui_values.base_dim.z = new_depth
--- building.gd
+++ building.gd
@@ -1,20 +1,60 @@
 extends Node3D
 
+
 var _data
 
 @export var ANIM_SPEED = 5
 var foundation: MeshInstance3D
+
+var building = {}
+
+func set_dim(sizeable, new_size, delta):
+	sizeable.size = sizeable.size.lerp(new_size, delta * ANIM_SPEED)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_data = get_node("../_data")
 
 	print(_data.ui_values)
+	create_wall("left_wall")
+	create_wall("back_wall")
+	
+	print(building)
 
 	foundation = $FoundationMesh
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	set_dim(foundation.mesh, Vector3(_data.ui_values.base_dim.x, foundation.mesh.size.y, _data.ui_values.base_dim.z), delta)
+
+	position_back_wall(delta)
+	#position_wall("left_wall", delta)
 	
-	foundation.mesh.size = foundation.mesh.size.lerp(Vector3(_data.ui_values["width"], foundation.mesh.size.y, _data.ui_values["depth"]), delta * ANIM_SPEED)
+
+func position_back_wall(delta):
+	var base = building.back_wall.base_board
+	#base.mesh.size = Vector3(_data.ui_values.board_dim.z, _data.ui_values.board_dim.y, _data.ui_values.base_dim.z), delta)
+	set_dim(base.mesh, Vector3(_data.ui_values.board_dim.z, _data.ui_values.board_dim.y, _data.ui_values.base_dim.z), delta)
+	base.position = Vector3(-(foundation.mesh.size.x / 2) + (_data.ui_values.board_dim.z / 2), foundation.mesh.size.y + (_data.ui_values.board_dim.y / 2), 0)
+
+func position_wall(wall_name, delta):
+	var base = building[wall_name].base_board
+	set_dim(base.mesh, Vector3(_data.ui_values.base_dim.x - (_data.ui_values.board_dim.z * 2), _data.ui_values.board_dim.y, _data.ui_values.board_dim.z), delta)
+	base.position = Vector3(0, foundation.mesh.size.y + (_data.ui_values.board_dim.y / 2), (foundation.mesh.size.z / 2) - (_data.ui_values.board_dim.z / 2))
+	
+
+# Instantiate
+
+func create_wall(wall_name):
+	building[wall_name] = {}
+	building[wall_name].base_board = create_new_board(Vector3.ZERO)
+	
+
+var board_scene = preload("res://components/board.tscn")
+func create_new_board(the_position):
+	var new_instance = board_scene.instantiate()
+	new_instance.position = the_position
+	self.add_child(new_instance)
+	return new_instance
